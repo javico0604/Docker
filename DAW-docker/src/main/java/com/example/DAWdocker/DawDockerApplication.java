39ed4eab7edd35048c783cbe07bcee38e214aab3
javico0604 <115450107+javico0604@users.noreply.github.com>
2024-02-05 10:17:20 +0100

    Add files via upload

---
+++ DAW-docker/src/main/java/com/example/DAWdocker/DawDockerApplication.java
@@ -0,0 +1,13 @@
+package com.example.DAWdocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DawDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DawDockerApplication.class, args);
+	}
+
+}
---
+++ DAW-docker/src/test/java/com/example/DAWdocker/DawDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DAWdocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DawDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
